1d1fec80b027ce7b27ca752f96d8871d9f934e78
gayratjon <gayratjonr@gmail.com>
2019-08-20 19:15:34 +0500

    commit first solution

---
+++ problem/src/solution/Problem1.java
@@ -0,0 +1,17 @@
+package solution;
+
+import java.util.Scanner;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum  = 0;
+        for (int i = 3; i < n; i++) {
+            if (i % 3 == 0 || i % 5 == 0){
+                sum += i;
+            }
+            System.out.println(sum);
+        }
+    }
+}
